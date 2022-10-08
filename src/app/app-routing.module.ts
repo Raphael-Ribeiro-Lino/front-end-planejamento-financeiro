86620958873e78d5e112dc74f7ec2bcf86edbdf7
Raphael Ribeiro Lino <raphar.lino@gmail.com>
2022-10-08 19:21:03 -0300

    Adicionando rotas de entradas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormularioEntradasComponent } from './entradas/formulario-entradas/formulario-entradas.component';
+import { ListaEntradasComponent } from './entradas/lista-entradas/lista-entradas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "entradas", component: ListaEntradasComponent},
+  {path: "entradas/formulario", component: FormularioEntradasComponent},
+  {path: "entradas/formulario/:id", component: FormularioEntradasComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
