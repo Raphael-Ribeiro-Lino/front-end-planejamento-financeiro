1a5ad61ea4123f2db8085d5626eb708da2e939b6
Raphael Ribeiro Lino <raphar.lino@gmail.com>
2022-10-09 23:16:51 -0300

    Adicionando formulario custo fixo

--- src/app/custos-fixos/formulario-custos-fixos/formulario-custos-fixos.component.ts
+++ src/app/custos-fixos/formulario-custos-fixos/formulario-custos-fixos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CustoFixoService } from '../custo-fixo.service';
 
 @Component({
   selector: 'app-formulario-custos-fixos',
@@ -6,8 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario-custos-fixos.component.css']
 })
 export class FormularioCustosFixosComponent implements OnInit {
+  id: number | null;
 
-  constructor() { }
+  custoFixoFormGroup: FormGroup;
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private custoFixoService: CustoFixoService
+  ) {
+    this.id = this.activateRoute.snapshot.paramMap.get("id") as number | null;
+    this.custoFixoFormGroup = this.formBuilder.group({
+      nome: ['', Validators.required],
+      valor: ['', Validators.required],
+    });
+   }
 
   ngOnInit(): void {
   }
